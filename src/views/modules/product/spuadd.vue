014cc317932a3680a8a8103f7d363e0b1431676e
wangb1in <1178321785@qq.com>
2021-01-01 15:32:28 +0800

    feat:商品服务-API 新增商品
    -- 调试会员等级相关接口
    -- 获取分类关联的品牌
    -- 获取分类所有分组以及属性
    -- 商品新增VO抽取
    -- 商品新增业务流程分析
    -- 保存spu基本信息
    -- 保存sku基本信息
    -- 调用远程服务保存优惠等信息
    -- SPU检索
    -- SKU检索

--- src/views/modules/product/spuadd.vue
+++ src/views/modules/product/spuadd.vue
@@ -392,12 +392,12 @@ export default {
         brandId: [
           { required: true, message: "请选择一个品牌", trigger: "blur" }
         ],
-        decript: [
-          { required: true, message: "请上传商品详情图集", trigger: "blur" }
-        ],
-        images: [
-          { required: true, message: "请上传商品图片集", trigger: "blur" }
-        ],
+        // decript: [
+        //   { required: true, message: "请上传商品详情图集", trigger: "blur" }
+        // ],
+        // images: [
+        //   { required: true, message: "请上传商品图片集", trigger: "blur" }
+        // ],
         weight: [
           {
             type: "number",
@@ -458,13 +458,13 @@ export default {
         brandId: "",
         weight: "",
         publishStatus: 0,
-        decript: [], 
-        images: [], 
+        decript: [],
+        images: [],
         bounds: {
           buyBounds: 0,
           growBounds: 0
         },
-        baseAttrs: [], 
+        baseAttrs: [],
         skus: []
       };
     },
